841e5c551df683fe1ba3515d7217240428ec5b00
ilymxrw <133024090+ilymxrw@users.noreply.github.com>
2024-10-04 10:54:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o primeiro presidente do Brasil após a monarquia?',
+    'Marechal Deodoro'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
